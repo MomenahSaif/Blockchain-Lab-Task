4cf75462f11201cb0858dbb542181c460588cdf0
MomenahSaif <147816133+MomenahSaif@users.noreply.github.com>
2024-09-30 02:16:22 -0400

    Update main.go

--- Assignment 1/main.go
+++ Assignment 1/main.go
@@ -1,1 +1,30 @@
+package main
 
+import "fmt"
+
+func main() {
+    // Creating the first block (genesis block)
+    genesisBlock := NewBlock("genesis to network", 1, "0")
+    fmt.Println("Genesis Block Created:")
+    ListBlocks()
+
+    // Adding more blocks
+    NewBlock("bob to alice", 2, genesisBlock.Hash)
+    NewBlock("alice to charlie", 3, genesisBlock.Hash)
+
+    fmt.Println("\nAfter Adding More Blocks:")
+    ListBlocks()
+
+    // Modifying a block
+    fmt.Println("\nModifying the second block:")
+    ChangeBlock(1, "bob to dave")
+    ListBlocks()
+
+    // Verifying the integrity of the chain
+    fmt.Println("\nVerifying Blockchain Integrity:")
+    if VerifyChain() {
+        fmt.Println("Blockchain is valid.")
+    } else {
+        fmt.Println("Blockchain is invalid!")
+    }
+}
